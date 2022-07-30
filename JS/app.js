db77151e0d1a39845e4acf917e0562ecaa3d4a66
sakibcse570 <17201122@uap-bd.edu>
2022-07-30 12:13:30 +0600

    Added ShowProfile function from UI class

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,6 @@
 let searchBtn = document.querySelector('#searchBtn');
 let searchUser = document.querySelector('#searchUser');
+let ui = new UI();
 
 searchBtn.addEventListener('click', (e) => {
     let userText = searchUser.value;
@@ -14,6 +15,7 @@ searchBtn.addEventListener('click', (e) => {
                     //Show Alert
                 } else {
                     //Show Profile
+                    ui.showProfile(data);
                 }
             })
     } else {
